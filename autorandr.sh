b8bfc149cfa538a6978d29b9287e06a8ed57640a
thapakazi <milans.thapa78@gmail.com>
2018-04-13 09:48:02 +0545

    adjust to my setting... thanks to orginal author :)

--- autorandr.sh
+++ autorandr.sh
@@ -14,10 +14,10 @@ EXTERNAL_MONITOR_STATUS=$( cat /sys/class/drm/card0-DP-1/status )
 # Is the external monitor connected?
 if [ "$EXTERNAL_MONITOR_STATUS" = "connected" ]; then
     TYPE="double"
-    xrandr --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal
+    xrandr --output DP1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --primary --mode  1368x768 --pos 0x1080 --rotate normal
 else
     TYPE="single"
-    /usr/bin/xrandr --output DP1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal
+    /usr/bin/xrandr --output eDP1 --off --output eDP1 --mode 1368x768 --pos 0x0 --rotate normal
 fi
 
 logger -t autorandr "Switched to $TYPE monitor mode"
